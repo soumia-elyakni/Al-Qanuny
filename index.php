2a7a3672d023eaa691d78f54c4d3bdd11135e296
soumia elyakni <selyakni@gmail.com>
2022-06-04 22:12:19 +0100

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    require_once './controllers/HomeController.php';
+
+    $home = new HomeController();
+
+    $pages = ['home', 'Tableau'];
+
+    if(isset($_GET['page'])){
+        if(in_array($_GET['page'],$pages)){
+            $page= $_GET['page'];
+            $home->index($page);
+        }else{
+            include('views/includes/404.php');
+        }
+    }else{
+        $home->index('home');
+    }
+?>
