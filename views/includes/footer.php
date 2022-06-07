2c38e51b14b1f4c77f841339c5b040a67a98dcfb
soumia elyakni <selyakni@gmail.com>
2022-06-07 22:16:11 +0100

    headerFootersLinks

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,3 +1,9 @@
+
+<div class="bg-light d-flex flex-between">
+ <p>Create bya Soumia EL YAKNI 2022</p>
+ <a href="../loginAdmin">Admin</a>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
