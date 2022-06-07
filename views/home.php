454a7b6fecdf343e066b7844bdf5919627622b51
soumia elyakni <selyakni@gmail.com>
2022-06-07 11:43:39 +0100

    tastAfficheimg

--- views/home.php
+++ views/home.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,500i,700,800i" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Qanuny-القانوني</title>
     <style>
     .rtl {
